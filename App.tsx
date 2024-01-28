9e36627df002981c2aea8a602e08b02bd01f0e18
Anil Piparaiya <anilkumarpiparaiya@gmail.com>
2024-01-29 00:33:03 +0530

    added new project directory

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
 
 function App() {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello World!</Text>
+      <View style={styles.container }>
+        <Login />
+        
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default App;
